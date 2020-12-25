10b60a6aaf91fb08c103e7647f6989b6bbe37b04
adamConnerSax <adam_conner_sax@yahoo.com>
2020-12-24 20:06:48 -0500

    Working...

--- init.el
+++ init.el
@@ -12,8 +12,12 @@
 (package-initialize)
 
 ;; HELM
-(require 'helm)
-(require 'helm-config)
+(use-package helm
+  :ensure t)
+
+;;(use-package helm-config
+;;  :ensure t
+;;  )
 
 
 ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
@@ -54,7 +58,9 @@
   )
 ;; some additions 12/24/2020
 (setq gc-cons-threshold 100000000)
-(use-package gnu-elpa-keyring-update)
+(use-package
+  :ensure t
+  gnu-elpa-keyring-update)
 (delete-selection-mode t)
 (global-display-line-numbers-mode t)
 (column-number-mode)
@@ -68,10 +74,12 @@
 (unbind-key "<mouse-2>") ;; pasting with mouse-wheel click
 (unbind-key "<C-wheel-down>") ;; text scale adjust
 (use-package keychain-environment
+  :ensure t
   :config
   (keychain-refresh-environment))
 
 (use-package undo-tree
+  :ensure t
   :diminish
   :bind (("C-c _" . undo-tree-visualize))
   :config
@@ -80,15 +88,19 @@
 
 (ignore-errors (set-frame-font "Menlo-12"))
 
-(use-package all-the-icons)
+(use-package all-the-icons
+  :ensure t
+  )
 
 (use-package all-the-icons-dired
+  :ensure t
   :after all-the-icons
   :hook (dired-mode . all-the-icons-dired-mode))
 
 (use-package doom-themes
+  :ensure t
   :config
-  (let ((chosen-theme 'doom-oceanic-next))
+  (let ((chosen-theme 'doom-one-light))
     (doom-themes-visual-bell-config)
     (doom-themes-org-config)
     (setq doom-challenger-deep-brighter-comments t
@@ -96,15 +108,18 @@
     (load-theme chosen-theme)))
 
 (use-package dimmer
+  :ensure t
   :custom (dimmer-fraction 0.1)
   :config (dimmer-mode))
 
 (show-paren-mode)
 
 (use-package rainbow-delimiters
+  :ensure t
   :hook ((prog-mode . rainbow-delimiters-mode)))
 
 (use-package magit
+  :ensure t
   :diminish magit-auto-revert-mode
   :diminish auto-revert-mode
   :bind (("C-c g" . #'magit-status))
@@ -114,6 +129,7 @@
   (add-to-list 'magit-no-confirm 'stage-all-changes))
 
 (use-package forge
+  :ensure t
   :after magit)
 ;;
 
@@ -197,8 +213,11 @@
 (setq temporary-file-directory "~/.emacs.d/tmp/")
 
 ;; Stan
-(require 'stan-mode)
+(use-package stan-mode
+  :ensure t
+  )
 (use-package company-stan
+  :ensure t
   :hook (stan-mode . company-stan-setup))
 
 
@@ -234,9 +253,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#f0f0f0" "#e45649" "#50a14f" "#986801" "#4078f2" "#a626a4" "#0184bc" "#1b2229"])
  '(column-number-mode t)
  '(custom-enabled-themes '(tsdh-light))
+ '(custom-safe-themes
+   '("b54826e5d9978d59f9e0a169bbd4739dd927eead3ef65f56786621b53c031a7c" default))
  '(dimmer-fraction 0.1)
+ '(fci-rule-color "#383a42")
  '(flycheck-haskell-hlint-executable "~/.cabal/bin/hlint")
  '(flycheck-hlintrc nil)
  '(haskell-mode-contextual-import-completion nil)
@@ -251,6 +275,9 @@
  '(haskell-process-type 'stack-ghci)
  '(haskell-stylish-on-save nil)
  '(haskell-tags-on-save t)
+ '(jdee-db-active-breakpoint-face-colors (cons "#f0f0f0" "#4078f2"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#f0f0f0" "#50a14f"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#f0f0f0" "#9ca0a4"))
  '(lsp-enable-file-watchers nil)
  '(lsp-file-watch-ignored
    '("[/\\\\]dist" "[/\\\\]dist-newstyle" "[/\\\\]\\.git$" "[/\\\\]\\.hg$" "[/\\\\]\\.bzr$" "[/\\\\]_darcs$" "[/\\\\]\\.svn$" "[/\\\\]_FOSSIL_$" "[/\\\\]\\.idea$" "[/\\\\]\\.ensime_cache$" "[/\\\\]\\.eunit$" "[/\\\\]node_modules$" "[/\\\\]\\.fslckout$" "[/\\\\]\\.tox$" "[/\\\\]\\.stack-work$" "[/\\\\]\\.bloop$" "[/\\\\]\\.metals$" "[/\\\\]target$" "[/\\\\]\\.deps$" "[/\\\\]build-aux$" "[/\\\\]autom4te.cache$" "[/\\\\]\\.reference$"))
@@ -259,7 +286,29 @@
  '(lsp-print-performance t)
  '(magit-repository-directories '(("~/src" . 1)))
  '(package-selected-packages
-   '(rainbow-delimiters all-the-icons dimmer doom-themes forge gnu-elpa-keyring-update keychain-environment flycheck-stan company-stan lsp-mode lsp-ui lsp-haskell magithub magit flycheck-pos-tip flycheck-hlint cl-lib shm helm haskell-mode flycheck-hdevtools flycheck-haskell flycheck-stack paredit ghci-completion ghc-imported-from flycheck company-ghc company)))
+   '(all-the-icons-dired undo-tree helm-config rainbow-delimiters all-the-icons dimmer doom-themes forge gnu-elpa-keyring-update keychain-environment flycheck-stan company-stan lsp-mode lsp-ui lsp-haskell magithub magit flycheck-pos-tip flycheck-hlint cl-lib shm helm haskell-mode flycheck-hdevtools flycheck-haskell flycheck-stack paredit ghci-completion ghc-imported-from flycheck company-ghc company))
+ '(vc-annotate-background "#fafafa")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#50a14f")
+    (cons 40 "#688e35")
+    (cons 60 "#807b1b")
+    (cons 80 "#986801")
+    (cons 100 "#ae7118")
+    (cons 120 "#c37b30")
+    (cons 140 "#da8548")
+    (cons 160 "#c86566")
+    (cons 180 "#b74585")
+    (cons 200 "#a626a4")
+    (cons 220 "#ba3685")
+    (cons 240 "#cf4667")
+    (cons 260 "#e45649")
+    (cons 280 "#d2685f")
+    (cons 300 "#c07b76")
+    (cons 320 "#ae8d8d")
+    (cons 340 "#383a42")
+    (cons 360 "#383a42")))
+ '(vc-annotate-very-old-color nil))
 
 (add-hook 'haskell-mode-hook 'haskell-hook)
 (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
